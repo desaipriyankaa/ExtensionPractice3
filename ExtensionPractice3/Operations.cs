83cab92bb80e5a652a1d12bbf19731ba9e9825d6
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-06-03 12:09:18 +0530

    Add project files.

---
+++ ExtensionPractice3/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExtensionPractice3
+{
+    public static class Operations
+    {
+        public static void add(this int num,int b)
+        {
+            Console.WriteLine("Addition");
+            int opr = num + b;
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Addition is : {opr}");
+            Console.ForegroundColor = defaultColor;  
+        }
+
+        public static void sub(this int num, int b)
+        {
+            Console.WriteLine("Subtraction");
+            int opr = num - b;
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Subtraction is : {opr}");
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+}
---
+++ ExtensionPractice3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtensionPractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            number.add(5);
+            number.sub(5);
+        }
+    }
+}
